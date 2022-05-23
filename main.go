bd4f99eff7813ed46e8a3d3f103958010e90116b
TarasG009 <taras.grinchuk22@gmail.com>
2022-05-23 15:27:31 +0300

    First commit!

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var g float32 = 35 / 3.0
+	//var Hello string
+	//number := 10
+	//var a int = 8
+	//var b int = 3
+	//var c bool = a == b
+	//Hello = "Hallo \r World"
+	//
+	//fmt.Println(Hello)
+	//fmt.Println(number)
+	//fmt.Println(c)
+	//fmt.Printf("sdfg\\:hjkl;")
+	//fmt.Printf("TTT\n")
+	//fmt.Println(g)
+
+	//var a = 35
+	//var b = 3
+	//var c = a / b
+	//var d = a % b
+	//
+	//fmt.Println(c, ".", d)
+	////fmt.Print(d)
+
+	//var bool = 4 < 5 && 6 < 8
+	//
+	//fmt.Println(bool)
+
+	A := 8
+	B := 16
+	C := 32
+	D := 64
+	E := 128
+	F := 256
+	G := 512
+	H := 1024
+	I := 2048
+	J := 4096
+
+	fmt.Println(A, B, C, D, E, F, G, H, I, J)
+
+}
