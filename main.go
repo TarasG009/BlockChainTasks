f8d8b9b2b6147bb8c493bdcef7a310ed06a105a1
TarasG009 <106081153+TarasG009@users.noreply.github.com>
2022-05-26 22:10:59 +0300

    Add files via upload
    Task 1 completed.

--- main.go
+++ main.go
@@ -1,46 +1,99 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+	"time"
+)
+
+var AllKeys [][]byte
 
 func main() {
-	//var g float32 = 35 / 3.0
-	//var Hello string
-	//number := 10
-	//var a int = 8
-	//var b int = 3
-	//var c bool = a == b
-	//Hello = "Hallo \r World"
-	//
-	//fmt.Println(Hello)
-	//fmt.Println(number)
-	//fmt.Println(c)
-	//fmt.Printf("sdfg\\:hjkl;")
-	//fmt.Printf("TTT\n")
-	//fmt.Println(g)
+	All()
 
-	//var a = 35
-	//var b = 3
-	//var c = a / b
-	//var d = a % b
-	//
-	//fmt.Println(c, ".", d)
-	////fmt.Print(d)
+	num8, _ := GenerateRandomBytes(8)
+	num16, _ := GenerateRandomBytes(16)
+	num32, _ := GenerateRandomBytes(32)
+	num64, _ := GenerateRandomBytes(64)
+	num128, _ := GenerateRandomBytes(128)
+	num256, _ := GenerateRandomBytes(256)
+	num512, _ := GenerateRandomBytes(512)
+	num1024, _ := GenerateRandomBytes(1024)
+	num2048, _ := GenerateRandomBytes(2048)
+	num4096, _ := GenerateRandomBytes(4096)
 
-	//var bool = 4 < 5 && 6 < 8
-	//
-	//fmt.Println(bool)
+	fmt.Println(num8)
+	fmt.Println(num16)
+	fmt.Println(num32)
+	fmt.Println(num64)
+	fmt.Println(num128)
+	fmt.Println(num256)
+	fmt.Println(num512)
+	fmt.Println(num1024)
+	fmt.Println(num2048)
+	fmt.Println(num4096)
 
-	A := 8
-	B := 16
-	C := 32
-	D := 64
-	E := 128
-	F := 300
-	G := 512
-	H := 1024
-	I := 2048
-	J := 4096
+	fmt.Println("Keys -- ", AllKeys)
 
-	fmt.Println(A, B, C, D, E, F, G, H, I, J)
+	BootForce(num32)
+
+}
+
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+	AllKeys = append(AllKeys, b)
+
+	return b, nil
+}
+
+func BootForce(key []byte) {
+	start := time.Now()
+	duration := time.Since(start)
+	for i, n := range AllKeys {
+		if key[i] == n[i] {
+			fmt.Println("It is your key", n)
+			log.Print(duration)
+		}
+	}
+
+}
+
+func All() {
+	Vint8 := math.Pow(2, 8)
+	Vint16 := math.Pow(2, 16)
+	Vint32 := math.Pow(2, 32)
+	Vrint32 := int64(Vint32)
+	Vint64 := math.Pow(2, 64)
+	Vrint64 := uint64(Vint64)
+	Vint128 := big.NewInt(9223372036854775807) // int64
+	Vint128.Mul(Vint128, big.NewInt(9223372036854775807))
+	Vint256 := Vint128
+	Vint256.Mul(Vint256, Vint128)
+	Vint512 := Vint256
+	Vint512.Mul(Vint512, Vint256)
+	Vint1024 := Vint512
+	Vint1024.Mul(Vint1024, Vint512)
+	Vint2048 := Vint1024
+	Vint2048.Mul(Vint2048, Vint1024)
+	Vint4096 := Vint2048
+	Vint4096.Mul(Vint4096, Vint2048)
+
+	fmt.Println(Vint8)
+	fmt.Println(Vint16 - 1)
+	fmt.Println(Vrint32 - 1)
+	fmt.Println(Vrint64 - 1)
+	fmt.Println(Vint128)
+	fmt.Println(Vint256)
+	fmt.Println(Vint512)
+	fmt.Println(Vint1024)
+	fmt.Println(Vint2048)
+	fmt.Println(Vint4096)
 
 }
